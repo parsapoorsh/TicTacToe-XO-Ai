d5290ba5fb60ba53dc172959de99a9db4a3c2684
Parsa <parsa.poorsh@gmail.com>
2023-02-09 15:19:02 +0330

    SUPER cleaning and reformatting

--- XO.py
+++ XO.py
@@ -1,85 +1,86 @@
-from functools import lru_cache
 from copy import copy
-from typing import Tuple, List, Iterable, Union
+from functools import lru_cache
 from math import inf, ceil
+from typing import Tuple, Iterable, Union
 
-class players():
+
+class PLAYERS:
     X = -1
     O = 1
 
+
+# noinspection PyUnresolvedReferences
 class Board(list):
-    __all__ = ('check', 'ai_move', 'empty_cells', 'cell_for_check', 'score2status', 'move', 'is_win', 'is_tie', 'is_end', 'clear', 'copy')
-    def __init__(self, size: int=3):
-        self.size=size
-        super().__init__([None]*size*size)
+    __all__ = ('ai_move', 'empty_cells', 'cell_for_check', 'move', 'is_win', 'is_tie', 'is_end', 'clear', 'copy')
+
+    def __init__(self, size: int = 3):
+        self.size = size
+        super().__init__([None] * size * size)
 
     @staticmethod
     @lru_cache
-    def cell_for_check(n) -> Tuple[Tuple[int]]:
+    def cell_for_check(n):
         reuslt = []
-        reuslt.extend(tuple(i+ii-1 for i in range(1, n*n+1, n)) for ii in range(n)) # -
-        reuslt.extend(tuple(i+ii-1 for i in range(n)) for ii in range(1, n*n+1, n)) # | 
-        reuslt.append(tuple(i*n - (n-i)-1 for i in range(1, n+1)))                  # \
-        reuslt.append(tuple(i*n - (i-1)-1 for i in range(1, n+1)))                  # /
+        reuslt.extend(tuple(i + ii - 1 for i in range(1, n * n + 1, n)) for ii in range(n))  # -
+        reuslt.extend(tuple(i + ii - 1 for i in range(n)) for ii in range(1, n * n + 1, n))  # |
+        reuslt.append(tuple(i * n - (n - i) - 1 for i in range(1, n + 1)))  # \
+        reuslt.append(tuple(i * n - (i - 1) - 1 for i in range(1, n + 1)))  # /
         return tuple(reuslt)
 
     def empty_cells(self) -> Iterable[int]:
-        return [i for i in range(self.size*self.size) if self[i] == None]
+        return [i for i in range(self.size * self.size) if self[i] is None]
 
     # TODO: make it async
-    def ai_move(self, player: int) -> Tuple[int, Union[int, float], int]:
-        """Move the player with ai, returns move, score, depth"""
+    def ai_move(self, player: int) -> Tuple[int, Union[float, int]]:
+        """Move the player with AI, returns move, score, depth"""
         assert not self.is_end(), 'Game is End'
         best_score = -inf
         best_move = None
-        max_depth = max_depth if max_depth != None else self.size*self.size
 
-        if self.count(None) == self.size*self.size:
+        if self.count(None) == self.size * self.size:
             # if start first, use centeral cell
-            best_move = ceil(self.size/2) * self.size - (self.size - ceil(self.size/2)) -1
+            best_move = ceil(self.size / 2) * self.size - (self.size - ceil(self.size / 2)) - 1
             self[best_move] = player
-            return best_move, inf, self.size*self.size
+            return best_move, inf
 
         for key in self.empty_cells():
             self[key] = player
-            score, depth = self.alpha_beta(player)
+            score: int = self.alpha_beta(player)
             self[key] = None
             if score > best_score:
                 best_score = score
                 best_move = key
-            if score == 1: # if is win move, break the loop
+            if score == 1:  # if is win move, break the loop
                 break
-        
+
         self[best_move] = player
-        return best_move, best_score, max_depth-depth+1
+        return best_move, best_score
 
     def move(self, key: int, player: int):
         key -= 1
-        assert self[key] == None, 'Bad cell'
+        assert self[key] is None, 'Bad cell'
         self[key] = player
         return self
 
     def alpha_beta(
             self,
             player: int,
-            depth: int,
             alpha: Union[int, float] = -inf,
             beta: Union[int, float] = inf,
             is_max: bool = False
-        )  -> Tuple[int, int]:
-        if self.is_win(player): # win
-            return 1, depth
-        elif self.is_win(-player): # lose
-            return -1, depth
-        elif self.is_tie() or depth <= 0: # tie
-            return 0, depth
-
+    ) -> int:
+        if self.is_win(player):  # win
+            return 1
+        elif self.is_win(-player):  # lose
+            return -1
+        elif self.is_tie():  # tie
+            return 0
 
         if is_max:
             best_score = -inf
             for key in self.empty_cells():
                 self[key] = player
-                score, d = self.alpha_beta(player, depth-1, alpha, beta, False)
+                score = self.alpha_beta(player, alpha, beta, False)
                 self[key] = None
                 if score > best_score:
                     best_score = score
@@ -87,12 +88,12 @@ class Board(list):
                     alpha = score
                 if beta <= alpha:
                     break
-            return best_score, d
+            return best_score
         else:
             worst_score = inf
             for key in self.empty_cells():
                 self[key] = -player
-                score, d = self.alpha_beta(player, depth-1, alpha, beta, True)
+                score = self.alpha_beta(player, alpha, beta, True)
                 self[key] = None
                 if score < worst_score:
                     worst_score = score
@@ -100,67 +101,28 @@ class Board(list):
                     beta = score
                 if beta <= alpha:
                     break
-            return worst_score, d
+            return worst_score
 
-    @staticmethod
-    def score2status(score: Union[int, float]) -> str:
-        return (
-            'tie' if score == 0 else
-            'win' if score > 0 else
-            'lose'
-        )
-    
     def is_win(self, player: int) -> bool:
         for row in self.cell_for_check(self.size):
             if all(self[i] == player for i in row):
                 return True
         return False
-    
+
     is_tie: bool = lambda self: self.count(None) == 0
-    is_end: bool = lambda self: self.is_tie() or self.is_win(players.O) or self.is_win(players.X)
+    is_end: bool = lambda self: self.is_tie() or self.is_win(PLAYERS.O) or self.is_win(PLAYERS.X)
 
     def clear(self):
-        '''Clear the board.'''
-        super().__init__([None]*self.size*self.size)
-    
+        """Clear the board."""
+        super().__init__([None] * self.size * self.size)
+
     def copy(self):
-        '''Return a shallow copy of the board.'''
+        """Return a shallow copy of the board."""
         return copy(self)
 
     def __repr__(self) -> str:
-        PLAYERS = {players.X: 'X', players.O: 'O', None: ' '}
-        ROW = f'+{"-"*3}' * self.size + '+\n'
-        string = ROW
-        for i in range(0, self.size*self.size, self.size):
-            string += '|' + '|'.join(f"{PLAYERS[i]:^3}" for i in self[i: i+self.size]) + f'|\n{ROW}'
+        pcha = {PLAYERS.X: 'X', PLAYERS.O: 'O', None: ' '}
+        string = row = f'+{"-" * 3}' * self.size + '+\n'
+        for i in range(0, self.size * self.size, self.size):
+            string += '|' + '|'.join(f"{pcha[i]:^3}" for i in self[i: i + self.size]) + f'|\n{row}'
         return string[:-1]
-
-if __name__ == '__main__':
-    from time import time
-    board = Board(3)
-    last_player = players.X
-    while 1:
-        t1 = time()
-        move, score, depth = board.ai_move(last_player, multiprocess=False)
-        t2 = time()
-        tT = round(t2-t1, 3)
-        print(
-            (
-                f'processed depth: {depth}, '
-                f'status: {board.score2status(score)}, '
-                f'move: {move+1}, '
-                f'{tT}s\n'
-            ),
-            board
-        )
-        
-        last_player = -last_player # convert players
-        if board.is_end():
-            print(
-                'Game is end,',
-                'Tie' if board.is_tie() else
-                'X Win' if board.is_win(players.X) else
-                'O Win'
-            )
-            #board.clear()
-            break
